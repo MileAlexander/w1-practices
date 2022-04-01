f21eb67df902930514a912cf8681309cfb7f8904
Mile Alexander <milealexander@gmail.com>
2022-04-01 17:55:39 +0200

    Pénteki munka

---
+++ frontend/script.js
@@ -0,0 +1,20 @@
+console.log("Ez az első javascriptem")
+/*
+// A stringeket zárójelbe kell írni.
+"Ez itt egy szöveg"
+// Dupla és sima macskaköröm csak egy soros stringet enged meg.
+'Ez itt egy másik szöveg'
+// Backtickek közé rakott stringben lehet sortörés.
+`Ez egy több
+ soros szöveg`
+// A számokat nem kell idézőjelbe rakni.
+4
+128
+04
+1.13
+*/
+function myFirstFunction(){
+    console.log("Ez az első függvényem belsejében levő log")
+}
+
+myFirstFunction()
